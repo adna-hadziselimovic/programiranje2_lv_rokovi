aefad09d86b01e4a8d0c6c7bd47d0c73b0d9e159
adna-hadz <148917625+adna-hadz@users.noreply.github.com>
2024-04-25 23:52:17 +0200

    Update rok11_z1.cpp

--- rok11/rok11_z1.cpp
+++ rok11/rok11_z1.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
 
+using namespace std;
+
+int najduzi_rastuci(vector<string>& v) {
+    int n = v.size();
+
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    int najduza = 1;
+    int trenutna = 1;
+
+    for (int i = 1; i < n; i++) {
+        if (v[i] > v[i - 1]) {
+            trenutna++;
+        } else {
+            najduza = max(najduza, trenutna);
+            trenutna = 1;
+        }
+    }
+    return max(najduza, trenutna);
+}
+
+int main() {
+    vector<string> niz = {"aaa", "bb", "c", "aac", "bbc", "bcc", "bcd", "bxx", "aa", "ggg", "xyz"};
+
+    cout << najduzi_rastuci(niz) << endl;
+    return 0;
+}
